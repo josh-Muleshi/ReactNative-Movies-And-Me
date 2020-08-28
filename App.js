ef7a47590de8d744b127e476ab50719c9bdd0ab7
josh muleshi <jmuleshi2@gmail.com>
2020-08-28 14:41:16 +0100

    hello word

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Salut josh</Text>
+      <Text>hello word !</Text>
       <StatusBar style="auto" />
     </View>
   );
