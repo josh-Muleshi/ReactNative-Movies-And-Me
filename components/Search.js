2b0b1dec99fae1f7ec2477b71c24b7cd9a74f75e
josh muleshi <jmuleshi2@gmail.com>
2020-08-29 12:05:29 +0100

    ajout du style et de la librairi stylesheet

--- components/Search.js
+++ components/Search.js
@@ -1,16 +1,27 @@
 import React from 'react';
-import { View, Button, TextInput, Text } from 'react-native';
+import { StyleSheet, View, Button, TextInput, Text } from 'react-native';
 
 class Search extends React.Component {
     render() {
         return(
-            <View>
-                <Text >Bienvenu</Text>
-                <TextInput placeholder="Titre du film" />
+            <View style={{ marginTop: 20 }}>
+                <TextInput style={styles.textinput} placeholder="Titre du film" />
                 <Button onPress={() => {}} title="Rechercher" />
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    textinput: {
+      marginLeft: 5,
+      marginRight: 5,
+      height: 50,
+      borderColor: '#000000',
+      borderWidth: 1,
+      paddingLeft: 5,
+      borderColor: 'gray'
+    }
+  })
 
 export default Search
