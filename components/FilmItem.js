431715c845cf44bd7f229e8ca7b739bcb7ef55a5
josh muleshi <jmuleshi2@gmail.com>
2020-09-02 18:31:24 +0100

    ajout du  templet dans notre filmItem.js

--- components/FilmItem.js
+++ components/FilmItem.js
@@ -1,11 +1,26 @@
 import React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, Image } from "react-native";
 
 class FilmItem extends React.Component {
   render() {
     return (
       <View style={styles.main_container}>
-        <Text style={styles.title_text}>Titre du film</Text>
+        <Image style={styles.image} source={{ uri: "image" }} />
+        <View style={styles.content_container}>
+          <View style={styles.header_container}>
+            <Text style={styles.title_text}>Titre du film</Text>
+            <Text style={styles.vote_text}>Vote</Text>
+          </View>
+          <View style={styles.description_container}>
+            <Text style={styles.description_text} numberOfLines={6}>
+              Description
+            </Text>
+            {/* La propriété numberOfLines permet de couper un texte si celui-ci est trop long, il suffit de définir un nombre maximum de ligne */}
+          </View>
+          <View style={styles.date_container}>
+            <Text style={styles.date_text}>Sorti le 00/00/0000</Text>
+          </View>
+        </View>
       </View>
     );
   }
@@ -14,8 +29,48 @@ class FilmItem extends React.Component {
 const styles = StyleSheet.create({
   main_container: {
     height: 190,
+    flexDirection: "row",
   },
-  title_text: {},
+  image: {
+    width: 120,
+    height: 180,
+    margin: 5,
+    backgroundColor: "gray",
+  },
+  content_container: {
+    flex: 1,
+    margin: 5,
+  },
+  header_container: {
+    flex: 3,
+    flexDirection: "row",
+  },
+  title_text: {
+    fontWeight: "bold",
+    fontSize: 20,
+    flex: 1,
+    flexWrap: "wrap",
+    paddingRight: 5,
+  },
+  vote_text: {
+    fontWeight: "bold",
+    fontSize: 26,
+    color: "#666666",
+  },
+  description_container: {
+    flex: 7,
+  },
+  description_text: {
+    fontStyle: "italic",
+    color: "#666666",
+  },
+  date_container: {
+    flex: 1,
+  },
+  date_text: {
+    textAlign: "right",
+    fontSize: 14,
+  },
 });
 
 export default FilmItem;
