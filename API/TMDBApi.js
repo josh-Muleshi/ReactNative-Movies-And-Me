754a5314bba8e692788e4937e956022b32f15781
josh muleshi <jmuleshi2@gmail.com>
2020-09-05 11:50:13 +0100

    appel de l'api

--- API/TMDBApi.js
+++ API/TMDBApi.js
@@ -1,3 +1,12 @@
 const API_TOKEN = "3c2b30c6bdbf696fb07dac09c811b6bc";
 
-export function getFilmsFromApiWithSearchedText(text) {}
+export function getFilmsFromApiWithSearchedText(text) {
+  const url =
+    "https://api.themoviedb.org/3/movie/550?api_key=" +
+    API_TOKEN +
+    "&language=fr&query=" +
+    text;
+  return fetch(url)
+    .then((Response) => Response.json())
+    .catch((Error) => console.log(Error));
+}
