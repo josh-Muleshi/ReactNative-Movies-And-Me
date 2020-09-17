3729d0317d3b7216f72691e4030a8d26d7f4776d
josh muleshi <jmuleshi2@gmail.com>
2020-09-17 17:23:16 +0100

    ajout d'une activite loading

--- components/Search.js
+++ components/Search.js
@@ -8,6 +8,7 @@ import {
   Button,
   Text,
   FlatList,
+  ActivityIndicator,
 } from "react-native";
 import films from "../Helpers/filmsData";
 import FilmItem from "./FilmItem";
@@ -18,14 +19,29 @@ class Search extends React.Component {
     super(props);
     this.state = {
       films: [],
+      isLoading: false,
     };
     this.searchedText = "";
   }
 
   _loadFilms() {
+    this.setState({ isLoading: true });
     if (this.searchedText.length > 0) {
       getFilmsFromApiWithSearchedText(this.searchedText).then((data) =>
-        this.setState({ films: data.results })
+        this.setState({
+          films: data.results,
+          isLoading: false,
+        })
+      );
+    }
+  }
+
+  _displayLoading() {
+    if (this.state.isLoading) {
+      return (
+        <View style={styles.loading_container}>
+          <ActivityIndicator size="large" />
+        </View>
       );
     }
   }
@@ -39,6 +55,7 @@ class Search extends React.Component {
     return (
       <View style={styles.main_container}>
         <TextInput
+          onSubmitEditing={() => this._loadFilms()}
           onChange={(text) => this._searchInputTextChanged(text)}
           style={styles.textinput}
           placeholder="Titre du film"
@@ -50,6 +67,7 @@ class Search extends React.Component {
           keyExtractor={(item) => item.id.toString()}
           renderItem={({ item }) => <FilmItem film={item} />}
         />
+        {this._displayLoading()}
       </View>
     );
   }
@@ -67,6 +85,15 @@ const styles = StyleSheet.create({
     borderColor: "#000000",
     borderWidth: 1,
     paddingLeft: 5,
+  },
+  loading_container: {
+    position: "absolute",
+    left: 0,
+    right: 0,
+    top: 100,
+    bottom: 0,
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
 
